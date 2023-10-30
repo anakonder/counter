acd36e1cae5413bb4f209b5ca7ec59e141509e89
anakonder <120507138+anakonder@users.noreply.github.com>
2023-10-30 17:48:14 +0200

    modal-form

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,7 +3,19 @@ import css from './Modal.module.css'
 export const Modal = () => {
     return(
         <div className={css.modalWraper}>
-            <div className={css.modalBody}>Modal
+            <div className={css.modalBody}>
+                <form className={css.loginForm}>
+                    <h2 className={css.loginTitle}>User autorisation</h2>
+                    <label className={css.loginLabel}>
+                        Email
+                        <input className={css.loginInput} type="email" name='email' placeholder='test@mail.com'/>
+                    </label>
+                    <label className={css.loginLabel}>
+                        Password
+                        <input className={css.loginInput} type="password" name='password'/>
+                    </label>
+                    <button className={css.loginBtn}>Submit</button>
+                </form>
             </div>
         </div>
     )
